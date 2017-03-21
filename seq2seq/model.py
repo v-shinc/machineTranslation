84d5ff223436c759c0e028d94283bc072e2d8476
Shini Chen <chensn456@163.com>
2017-03-21 16:59:18 +0800

    change placeholder's shape

--- seq2seq/model.py
+++ seq2seq/model.py
@@ -36,8 +36,10 @@ def ff_layer(inputs, dim, scope, activation=None):
     return res
 
 def _shift(tensor):
-    shape = [1] + [s.value for s in tensor.get_shape()[1:]]
-    res = tf.concat(0, [tf.zeros(shape), tf.scan(lambda r, x: x, tensor)[:-1]])
+    # tensor: [seq_len, batch_size, dim]
+    shape = [s.value for s in tensor.get_shape()]
+    shape[0] = 1
+    res = tf.concat(0, [tf.zeros(shape),  tensor[:-1]])
     return res
 
 def gru_layer(inputs, masks, dim):
@@ -62,7 +64,6 @@ def gru_layer(inputs, masks, dim):
 
 def cond_gru_layer(inputs, masks, context, init_state, dim):
     masks = tf.expand_dims(masks, 2)
-
     def _cond_gru_step(h_prev, (x, m)):
         with tf.variable_scope('cond_gru_layer'):
 
@@ -81,19 +82,23 @@ class Model:
     def __init__(self, params):
         batch_size = params['batch_size']
         max_len = params['max_len']
-        self.x = tf.placeholder(tf.int32, [max_len, batch_size], name='x')
-        self.x_mask = tf.placeholder(tf.float32, [max_len, batch_size], name='x_mask')
-        self.y = tf.placeholder(tf.int32, [max_len, batch_size], name='y')
-        self.y_mask = tf.placeholder(tf.float32, [max_len, batch_size], name='y_mask')
+        self.x = tf.placeholder(tf.int32, [None, max_len], name='x')
+        self.x_mask = tf.placeholder(tf.float32, [None, max_len], name='x_mask')
+        self.y = tf.placeholder(tf.int32, [None, max_len], name='y')
+        self.y_mask = tf.placeholder(tf.float32, [None, max_len], name='y_mask')
 
+        x = tf.transpose(self.x, [0, 1])
+        y = tf.transpose(self.y, [0, 1])
+        x_mask = tf.transpose(self.x_mask, [0, 1])
+        y_mask = tf.transpose(self.y_mask, [0, 1])
         with tf.variable_scope('word_embedding'):
             initializer = tf.contrib.layers.xavier_initializer(uniform=True, seed=None, dtype=tf.float32)
             x_embedding = tf.get_variable('x_embedding', [params['source_vocab_size'], params['word_dim']], initializer=initializer)
             y_embedding = tf.get_variable('y_embedding', [params['target_vocab_size'], params['word_dim']], initializer=initializer)
         # word embedding (source)
-        x_emb = tf.nn.embedding_lookup(x_embedding, self.x)
+        x_emb = tf.nn.embedding_lookup(x_embedding, x)
         # pass through encoder gru, recurrence here
-        self.hh = gru_layer(x_emb, self.x_mask, params['hidden_dim'])
+        self.hh = gru_layer(x_emb, x_mask, params['hidden_dim'])  # [seq_len, batch_size, hidden_dim]
         # last hidden state of encoder rnn will be used to initialize decoder rnn
 
         self.context = tf.squeeze(self.hh[-1:])
@@ -101,28 +106,25 @@ class Model:
         init_state = ff_layer(self.context, params['hidden_dim'], 'ff_state', tf.nn.tanh)
 
         # word embedding (target)
-        self.y_emb = tf.nn.embedding_lookup(y_embedding, self.y)
-        self.y_embed_shifted = _shift(self.y_emb)
-        # y_embed_shifted = tf.pack(_unpack(y_emb)[:-1])
-        # y_embed_shifted = tf.pad(y_embed_shifted, [[1, 0], [0, 0]], 'CONSTANT')  # [seq_len, batch_size, word_dim]
+        self.y_emb = tf.nn.embedding_lookup(y_embedding, y)
+        self.y_embed_shifted = _shift(self.y_emb) # [batch_size, seq_len, word_dim]
 
         # decoder - pass through the decoder gru, recurrence here
-        # self.ss_ = cond_gru_layer(self.y_embed_shifted, self.y_mask, context, init_state, params['hidden_dim'])  # [seq_len, batch_size, hidden_dim]
         with tf.variable_scope('decodert'):
-            self.ss_ = cond_gru_layer(self.y_embed_shifted, self.y_mask, self.context, init_state, params['hidden_dim'])
+            self.ss_ = cond_gru_layer(self.y_embed_shifted, y_mask, self.context, init_state, params['hidden_dim'])  # [seq_len, batch_size, hidden_dim]
         contexts = tf.expand_dims(self.context, 0)
         # compute word probabilities
-        logit_lstm = ff_layer(tf.pack(self.ss_), params['word_dim'], 'ff_logit_lstm')
-        logit_prev = ff_layer(self.y_embed_shifted, params['word_dim'], 'ff_logit_prev')
+        logit_lstm = ff_layer(self.ss_, params['word_dim'], 'ff_logit_lstm')
+        logit_prev = ff_layer(tf.transpose(self.y_embed_shifted, [0, 1]), params['word_dim'], 'ff_logit_prev')
         logit_ctx = ff_layer(contexts, params['word_dim'], 'ff_logit_ctx')
         logits = tf.nn.tanh(logit_lstm + logit_prev + logit_ctx)
         logits = ff_layer(logits, params['target_vocab_size'], 'ff_logit')  # [seq_len, batch_size, target_vocab_size]
 
         # cost
-        labels = tf.one_hot(self.y, params['target_vocab_size'])
+        labels = tf.one_hot(y, params['target_vocab_size'])
         probs = tf.reshape(tf.nn.softmax(tf.reshape(logits, [-1, params['target_vocab_size']])), [-1, batch_size, params['target_vocab_size']])
         cost = tf.log(tf.reduce_sum(labels * probs, 2))
-        self.cost = - tf.reduce_mean(tf.reduce_sum(cost * self.y_mask, 0))
+        self.cost = - tf.reduce_mean(tf.reduce_sum(cost * y_mask, 0))
 
         # train operator
         self.train_op = tf.train.GradientDescentOptimizer(params['lr']).minimize(self.cost)
